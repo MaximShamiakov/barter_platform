dec1f2d48cd13e1cce117538bfd74d8a62315732
maximax <shamiakov@bk.ru>
2025-05-26 18:49:56 +0200

    Implemented user registration: form, view, and template

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# ads/views.py
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация прошла успешно! Вы можете войти.')
+            return redirect('login')  # можно поменять на любую страницу
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
