7893ddbc9f600bd769338b34b0ad0b04e8e35ab6
maximax <shamiakov@bk.ru>
2025-05-27 12:50:46 +0200

    main login/logout links depending on authorization

--- ads/urls.py
+++ ads/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
      path('password-reset-complete/', 
           auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
           name='password_reset_complete'),
-     path('ads/', views.ads_list, name='ads_list'),
+     path('', views.ads_list, name='ads_list'),
 ]
