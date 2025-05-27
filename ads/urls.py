420008c41f5c1c83db4cb725071a7453ad63769f
maximax <shamiakov@bk.ru>
2025-05-27 08:42:30 +0200

    Implement password reset and recovery functionality using Django built-in views

--- ads/urls.py
+++ ads/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView 
+from django.contrib.auth import views as auth_views 
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logaut/', LogoutView.as_view(next_page='login'), name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+         name='password_reset'),
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('password-reset-complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
+         name='password_reset_complete'),
 ]
